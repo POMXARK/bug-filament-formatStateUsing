7a96fcf70a03b1c67e6522f56d6c7a1ed6ebf025
Роман Бушуев <bushuev@benefitty.ru>
2024-04-23 16:26:40 +0400

    init

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables\Table;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make('1 Section'),
+                Section::make('2 Section')
+                    ->schema([
+                        Forms\Components\TextInput::make('id')
+                            ->formatStateUsing(fn ($record) => 1)
+                    ]),
+                Section::make('3 Section'), // breaks the handler formatStateUsing
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table;
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
